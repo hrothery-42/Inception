398044103091db8988eaf023491b809c01506cf9
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-12 19:32:01 +0100

    give permissions to wp config file

--- srcs/requirements/wordpress/tools/wp_start.sh
+++ srcs/requirements/wordpress/tools/wp_start.sh
@@ -15,6 +15,8 @@ wp core download --allow-root
 
 #rename the sample config file as default config
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+chmod +x wp-config.php
 #change the important data in the config file
 sed -i -r "s/database_name_here/$DB_NAME/1"   wp-config.php
 sed -i -r "s/username_here/$DB_USER/1"  wp-config.php
@@ -22,11 +24,11 @@ sed -i -r "s/password_here/$DB_PWD/1"    wp-config.php
 sed -i -r "s/localhost/mariadb/1"    wp-config.php
 
 #install wordpress
-wp core install  --url=${WP_URL} \
-	--title=${WP_TITLE} \
-	--admin_user=${WP_ADMIN_LOGIN} \
-	--admin_password=${WP_ADMIN_PASSWORD} \
-	--admin_email=${WP_ADMIN_EMAIL} \
+wp core install  --url="${WP_URL}" \
+	--title="${WP_TITLE}" \
+	--admin_user="${WP_ADMIN_LOGIN}" \
+	--admin_password="${WP_ADMIN_PASSWORD}" \
+	--admin_email="${WP_ADMIN_EMAIL}" \
 	--skip-email --allow-root
 
 wp user create ${WP_USER_LOGIN} ${WP_USER_EMAIL} \
