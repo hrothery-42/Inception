20e0a4f78c8e1a5bb5fcc7a1801f30ece5692bbd
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-13 19:40:22 +0100

    keeps test in db

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -8,8 +8,6 @@ cat << EOF > /tmp/create_db.sql
 USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM     mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PWD}';
 CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
