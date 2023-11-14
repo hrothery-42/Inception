c0b4093cf47d22206c069acc10844a5fcaf15bad
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-14 18:55:28 +0100

    try new line to set password on root mysql

--- srcs/requirements/mariadb/tools/start_database.sh
+++ srcs/requirements/mariadb/tools/start_database.sh
@@ -11,8 +11,9 @@ echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db.sql
 echo "FLUSH PRIVILEGES;" >> db.sql
 
 #alters root password
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PWD' ;" >> db1.sql
-echo "ALTER USER 'root'@'%' IDENTIFIED BY '$ROOT_PWD' ;" >> db1.sql
+#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PWD' ;" >> db1.sql
+#echo "ALTER USER 'root'@'%' IDENTIFIED BY '$ROOT_PWD' ;" >> db1.sql
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$ROOT_PWD') ;" >> db1.sql
 
 #refreshes the db table with the new data
 echo "FLUSH PRIVILEGES;" >> db.sql
