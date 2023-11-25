ed14b65bfa8a446bf0feba613638d394bfac0b2c
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-25 18:35:05 +0100

    removed version again

--- srcs/requirements/wordpress/tools/wp_start.sh
+++ srcs/requirements/wordpress/tools/wp_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#if [ ! -d "/var/www/html" ]; then
-#	mkdir -p /var/www/html
-#fi
+if [ ! -d "/var/www/html" ]; then
+	mkdir -p /var/www/html
+fi
 
 cd /var/www/html
 
@@ -37,7 +37,7 @@ wp user create ${WP_USER_LOGIN} ${WP_USER_EMAIL} \
 	--allow-root --role=author 
 
 #listen on all ports so wordpress can connect outside its container
-#sed -i 's/listen = \/run\/php\/php-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i 's/listen = \/run\/php\/php-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 #sed -i "s|listen = 127.0.0.1:9000|listen = 9000|g" \
 #    /etc/php7/php-fpm.d/www.conf
 
