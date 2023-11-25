4060a6fb55f3545baaaa0b9bff075dba1a21b225
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-25 20:24:35 +0100

    reverted mariadb back to debian

--- srcs/requirements/mariadb/tools/start_database.sh
+++ srcs/requirements/mariadb/tools/start_database.sh
@@ -8,7 +8,6 @@ service mysql start
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD' ;" >> db.sql
 echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db.sql
-#echo "FLUSH PRIVILEGES;" >> db.sql
 
 #alters root password
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PWD' ;" >> db1.sql
