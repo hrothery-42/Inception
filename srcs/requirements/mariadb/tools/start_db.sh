e340286622208e2e741bcf9e9532e9ba071cf7a5
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-26 19:32:24 +0100

    grants privileges to root

--- srcs/requirements/mariadb/tools/start_db.sh
+++ srcs/requirements/mariadb/tools/start_db.sh
@@ -3,6 +3,7 @@
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > init.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';" >> init.sql
 echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" >> init.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY 'DB_PWD';" >> init.sql
 echo "FLUSH PRIVILEGES;" >> init.sql
 
 /usr/bin/mysqld --user=mysql --init-file=/tmp/init.sql
