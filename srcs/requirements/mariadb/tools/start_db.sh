8d72fc2129ae3ea9f9527308d0bf093ba71e6475
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-26 18:34:45 +0100

    changes * to dB_NAME

--- srcs/requirements/mariadb/tools/start_db.sh
+++ srcs/requirements/mariadb/tools/start_db.sh
@@ -2,7 +2,7 @@
 
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > init.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';" >> init.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';" >> init.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" >> init.sql
 echo "FLUSH PRIVILEGES;" >> init.sql
 
 /usr/bin/mysqld --user=mysql --init-file=/tmp/init.sql
