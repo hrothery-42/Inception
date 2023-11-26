c6c1df82e65b751a07b9e83c3879cd9249497fa1
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-26 18:28:53 +0100

    revises script mariadb

--- srcs/requirements/mariadb/tools/start_db.sh
+++ srcs/requirements/mariadb/tools/start_db.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 #!/bin/sh
 
-cat << EOF > /tmp/init.sql
+		cat << EOF > /tmp/init.sql
 
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PWD';
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';
-GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';"
-FLUSH PRIVILEGES;"
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
 
 mysqld --user=mysql --bootstrap < /tmp/init.sql
