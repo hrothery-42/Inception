12051e6b29d311a8a4d9466e1aeaf728965107bd
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-26 18:19:43 +0100

    reverted back to previous version.  uses --bootstrap instead of --init

---
+++ srcs/requirements/mariadb/tools/start_db.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#!/bin/sh
+
+cat << EOF > /init.sql
+
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';
+GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';"
+FLUSH PRIVILEGES;"
+
+mysqld --user=mysql --bootstrap < /init.sql
