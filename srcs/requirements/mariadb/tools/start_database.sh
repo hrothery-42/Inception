486712eb4d5f445ca99757182bc7b93c0c98a219
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-27 20:52:01 +0100

    change script

--- srcs/requirements/mariadb/tools/start_database.sh
+++ srcs/requirements/mariadb/tools/start_database.sh
@@ -4,21 +4,10 @@ mysqld_safe
 
 #creates a mysql script called db.sql
 
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD' ;" >> db.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db.sql
+mysql
 
-#alters root password
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PWD' ;" >> db1.sql
-
-#refreshes the db table with the new data
-echo "FLUSH PRIVILEGES;" >> db.sql
-
-#executes the SQL script in the MySql server
-mysql < db.sql
-
-#stops the MySql server
-kill $(cat /var/run/mysqld/mysqld.pid)
-
-#starts the server again
-mysqld
+CREATE DATABASE IF NOT EXISTS $DB_NAME ;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD' ;
+GRANT ALL ON *.* TO '$DB_USER'@'%' ;
+FLUSH PRIVILEGES;
+exit
