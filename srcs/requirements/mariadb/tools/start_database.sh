605b3ae65ef2b0deca5f47a4eaa15b54f1f18697
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-28 19:04:11 +0100

    adds mv cmd

--- srcs/requirements/mariadb/tools/start_database.sh
+++ srcs/requirements/mariadb/tools/start_database.sh
@@ -3,12 +3,11 @@
 
 #creates a mysql script called db.sql
 
-
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD' ;" >> db.sql
 echo "GRANT ALL ON *.* TO '$DB_USER'@'%' ;" >> db.sql
 echo "FLUSH PRIVILEGES;" >>db.sql
 
-mysql > db.sql
+mv db.sql /var/lib/mysql/db.sql
 mysqld_safe
 
