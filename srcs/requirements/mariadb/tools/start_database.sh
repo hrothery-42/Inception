0f2c188669ded8298a2e4a5b9a00d938494c44a6
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-28 18:55:20 +0100

    try another way script mariadb

--- srcs/requirements/mariadb/tools/start_database.sh
+++ srcs/requirements/mariadb/tools/start_database.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-mysqld_safe 
 
 #creates a mysql script called db.sql
 
-mysql
 
-CREATE DATABASE IF NOT EXISTS $DB_NAME ;
-CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD' ;
-GRANT ALL ON *.* TO '$DB_USER'@'%' ;
-FLUSH PRIVILEGES;
-exit
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD' ;" >> db.sql
+echo "GRANT ALL ON *.* TO '$DB_USER'@'%' ;" >> db.sql
+echo "FLUSH PRIVILEGES;" >>db.sql
+
+mysql > db.sql
+mysqld_safe
+
