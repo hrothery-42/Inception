c64a99f8c902806210abc9089cfec2bf77b845aa
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-28 19:31:03 +0100

    cleaned up code

--- srcs/requirements/mariadb/tools/start_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > init.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';" >> init.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" >> init.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY 'DB_PWD';" >> init.sql
-echo "FLUSH PRIVILEGES;" >> init.sql
-
-/usr/bin/mysqld --user=mysql --init-file=/tmp/init.sql
