194130997b38c57b0e98e2dbd14ded2dc90ccd5f
Heather Rothery <hrothery@students.42wolfsburg.de>
2023-11-28 19:12:03 +0100

    script works! all containers running without errors

--- srcs/requirements/mariadb/tools/start_database.sh
+++ srcs/requirements/mariadb/tools/start_database.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
-#creates a mysql script called db.sql
-
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD' ;" >> db.sql
 echo "GRANT ALL ON *.* TO '$DB_USER'@'%' ;" >> db.sql
 echo "FLUSH PRIVILEGES;" >>db.sql
 
 mv db.sql /
+
 mysqld_safe --init-file="/db.sql"
 
